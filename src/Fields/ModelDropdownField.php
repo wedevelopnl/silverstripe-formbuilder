1faa17a62a84598d06e9af08926e70da7d84765c
Mathijs Futselaar <mathijs@thewebmen.com>
2019-04-17 15:53:43 +0200

    Make allowed models in modeldropdownfield work

--- src/Fields/ModelDropdownField.php
+++ src/Fields/ModelDropdownField.php
@@ -3,21 +3,22 @@
 namespace TheWebmen\Formbuilder\Fields;
 
 use SilverStripe\ORM\ArrayList;
+use SilverStripe\View\ArrayData;
 
 class ModelDropdownField extends \SilverStripe\Forms\DropdownField
 {
     public static function get_allowed_models()
     {
-//        $models = self::config()->get('models');
-//
-//        $return = ArrayList::create();
-//
-//        foreach ($models as $model)
-//        {
-//            $return->push(new ArrayData(['class' => $model, 'name' => $model::$plural_name]));
-//        }
-//
-//        return $return;
+        $models = self::config()->get('models');
+
+        $return = ArrayList::create();
+
+        foreach ($models as $model)
+        {
+            $return->push(new ArrayData(['class' => $model, 'name' => (new $model())->plural_name()]));
+        }
+
+        return $return;
     }
 
 }
